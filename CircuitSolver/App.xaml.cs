111f7930f268f2483d4acd219adaf9b37f55638b
victoriaccmak <vmak@uwaterloo.ca>
2024-06-09 12:45:17 -0400

    Added my project

---
+++ CircuitSolver/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CircuitSolver
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CircuitSolver/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CircuitSolver
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
